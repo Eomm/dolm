b828e3aab67811354dc49f9fdbc16e605474ca35
Manuel Spigolon <behemoth89@gmail.com>
2017-04-21 22:10:19 +0200

    Added unit testing scaffolding

---
+++ src/test/java/it/eomm/plugins/dolm/test/DolmTest.java
@@ -0,0 +1,48 @@
+package it.eomm.plugins.dolm.test;
+
+import it.eomm.plugins.dolm.Dolm;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+
+import java.io.File;
+
+/**
+ * Created by Manuel Spigolon on 21/04/2017.
+ */
+public class DolmTest extends AbstractMojoTestCase {
+
+    /**
+     * {@inheritDoc}
+     */
+    protected void setUp() throws Exception {
+        // required
+        super.setUp();
+    }
+
+    public void testCorrectLoading() throws Exception {
+        File pom = getTestFile("src/test/resources/pom-test.xml");
+
+        assertNotNull(pom);
+        assertTrue(pom.exists());
+
+        Dolm mojo = (Dolm) lookupMojo("build-docs", pom);
+        assertNotNull(mojo);
+    }
+
+
+    /**
+     * TODO add assertions after execution
+     *
+     * @throws Exception
+     */
+    public void testExecution() throws Exception {
+        File pom = getTestFile("src/test/resources/pom-test.xml");
+
+        assertNotNull(pom);
+        assertTrue(pom.exists());
+
+        Dolm mojo = (Dolm) lookupMojo("build-docs", pom);
+        assertNotNull(mojo);
+
+        mojo.execute();
+    }
+}
